a21be62fc742ca3423c0b96baece4586558c19b5
apoorvpandey0 <apoorvpandey0@gmail.com>
2022-01-07 20:36:19 +0530

    resopnsiveness improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
     extend: {
       screens:{
         'max-md': {'max':'767px'},
+        'max-sm': {'max':'640px'},
+        'vsm': '480px',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
